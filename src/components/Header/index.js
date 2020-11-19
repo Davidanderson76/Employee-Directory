d99f63191ee1ffe7c0e121313f31e9577e1308a4
David Anderson <david.anderson1993@gmail.com>
2020-11-19 00:01:10 -0600

    updated some components, saving progress for the night

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>
+                Employee Directory
+            </h1>
+            <p>
+                Click on a header to filter the search, or use search bar!
+            </p>
+        </div>
+    );
+};
+
+export default Header;
+
--- src/utils/DataArea.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DataArea = React.createContect({
-
-});
-
-export default DataArea;
